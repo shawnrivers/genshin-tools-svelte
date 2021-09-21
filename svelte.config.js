3ea2909e035b52b0bd69b4d740796ad3dbcfb193
Shawn Rivers <kaushou@gmail.com>
2021-09-21 13:09:28 +0900

    Setup

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,6 @@
 import preprocess from 'svelte-preprocess';
+import adapter from '@sveltejs/adapter-static';
+import path from 'path';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -8,7 +10,20 @@ const config = {
 
 	kit: {
 		// hydrate the <div id="svelte"> element in src/app.html
-		target: '#svelte'
+		target: '#svelte',
+		vite: {
+			resolve: {
+				alias: {
+					$components: path.resolve('./src/components')
+				}
+			}
+		},
+		adapter: adapter({
+			// default options are shown
+			pages: 'build',
+			assets: 'build',
+			fallback: null
+		})
 	}
 };
 
